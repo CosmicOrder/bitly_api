2c2390b6f90104b2d80892b14f0550978a71a6e7
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-14 20:25:21 +0300

    add README, get rid of unnecessary class, split function into parts, move load_dotenv into ifmain, optimize code

--- main.py
+++ main.py
@@ -1,5 +1,54 @@
-from bitly_api import BitlyAPI
+import os
+from urllib.parse import urlparse
+
+import requests
+from dotenv import load_dotenv
+from requests import HTTPError
+
+
+def is_bitlink(link):
+    endpoint = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
+
+    response = requests.get(endpoint, headers=HEADERS)
+    return response.ok
+
+
+def shorten_link(link):
+    endpoint = 'https://api-ssl.bitly.com/v4/shorten'
+    payload = {"long_url": f"http://{link}"}
+
+    response = requests.post(endpoint, json=payload,
+                             headers=HEADERS)
+    response.raise_for_status()
+    bitlink = response.json()["link"]
+    return f'Битлинк: {bitlink}'
+
+
+def count_clicks(bitlink):
+    endpoint = f'https://api-ssl.bitly.com/v4/bitlinks/' \
+               f'{bitlink}/clicks/summary'
+
+    response = requests.get(endpoint, headers=HEADERS)
+    response.raise_for_status()
+    clicks_count = response.json()['total_clicks']
+    return f'По вашей ссылке прошли {clicks_count} раз(а)'
+
 
 if __name__ == '__main__':
-    bitly = BitlyAPI()
-    print(bitly.is_bitlink(input('Введите ссылку: ')))
+    load_dotenv()
+
+    token = os.getenv('BITLY_TOKEN')
+    HEADERS = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    url = input('Введите ссылку: ')
+    no_scheme_url = ''.join(urlparse(url)[1:])
+
+    try:
+        if is_bitlink(no_scheme_url):
+            print(count_clicks(no_scheme_url))
+        else:
+            print(shorten_link(no_scheme_url))
+    except HTTPError:
+        print('Проверьте правильность написания ссылки')
